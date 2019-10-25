463a690ff1813438e5580e0caed3aa1ddb762b5e
minaskar <minaskar@gmail.com>
2019-10-25 03:36:07 +0100

    added things into zeus.py

--- zeus/zeus.py
+++ zeus/zeus.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+
+class zeus:
+
+    def __init__(self, logp, nwalkers, ndim):
+        self.logp = logp
+        self.nwalkers = nwalkers
+        self.ndim = ndim
+
+
+    def run(self, start, nsteps):
