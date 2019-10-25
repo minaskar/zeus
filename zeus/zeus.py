22f923112154bdbf620596cbf9bd2db819ad894f
minaskar <minaskar@gmail.com>
2019-10-25 17:05:43 +0100

    defined run and _slice

--- zeus/zeus.py
+++ zeus/zeus.py
@@ -1,12 +1,31 @@
 import numpy as np
-
+from itertools import permutations
+from tqdm import tqdm
 
 class zeus:
 
-    def __init__(self, logp, nwalkers, ndim):
+    def __init__(self,
+                 logp,
+                 nwalkers,
+                 ndim):
+
         self.logp = logp
         self.nwalkers = nwalkers
         self.ndim = ndim
 
 
-    def run(self, start, nsteps):
+    def run(self,
+            start,
+            nsteps=1000,
+            width=1.0,
+            maxsteps=1,
+            thin=1):
+
+        dummy = 1.0
+
+
+    def _slice(self,
+               x,
+               direction):
+
+        dummy = 1.0
