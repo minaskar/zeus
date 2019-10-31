20ff80087019e20cbf63382d6b98411af82b634e
minaskar <minaskar@gmail.com>
2019-10-31 17:30:31 +0000

    added test_start.py

---
+++ zeus/tests/test_start.py
@@ -0,0 +1,18 @@
+#import sys
+#sys.path.append("./../../")
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import numpy as np
+import pytest
+
+from start import jitter
+
+def test_case1(seed=123):
+    np.random.seed(seed)
+    nwalkers = np.random.randint(1,300)
+    ndim = np.random.randint(1,300)
+    x = np.random.rand(nwalkers, ndim)
+    x_jittered = jitter(x, nwalkers, ndim)
+    assert np.allclose(x_jittered,x)
