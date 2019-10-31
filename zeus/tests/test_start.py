22bacb3f85e42e735508226dd83a3712015a27cc
minaskar <minaskar@gmail.com>
2019-10-31 18:42:51 +0000

    added two more tests for jitter

--- zeus/tests/test_start.py
+++ zeus/tests/test_start.py
@@ -9,10 +9,28 @@ import pytest
 from zeus import jitter
 
 
-def test_case1(seed=123):
+def test_case1(seed=42):
     np.random.seed(seed)
-    nwalkers = np.random.randint(1,300)
+    nwalkers = np.random.randint(2,300)
     ndim = np.random.randint(1,300)
     x = np.random.rand(nwalkers, ndim)
     x_jittered = jitter(x, nwalkers, ndim)
     assert np.allclose(x_jittered,x)
+
+
+def test_case2(seed=42):
+    np.random.seed(seed)
+    nwalkers = np.random.randint(2,300)
+    ndim = np.random.randint(1,300)
+    x = np.random.rand(ndim, walkers)
+    x_jittered = jitter(x, nwalkers, ndim)
+    assert np.allclose(x_jittered,x.T)
+
+
+def test_case3(seed=42):
+    np.random.seed(seed)
+    nwalkers = np.random.randint(2,300)
+    ndim = np.random.randint(1,300)
+    x = np.random.rand(ndim)
+    x_jittered = jitter(x, nwalkers, ndim)
+    assert np.shape(x_jittered)==(nwalkers,ndim)
