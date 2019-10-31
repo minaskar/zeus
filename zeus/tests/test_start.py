eca07908d06fc9e26b5abdf6bcd802321c931fa3
minaskar <minaskar@gmail.com>
2019-10-31 18:06:54 +0000

    testing zeus.start import

--- zeus/tests/test_start.py
+++ zeus/tests/test_start.py
@@ -1,13 +1,12 @@
-#import sys
-#sys.path.append("./../../")
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+#import sys, os
+#myPath = os.path.dirname(os.path.abspath(__file__))
+#sys.path.insert(0, myPath + '/../')
 
 import numpy as np
 import pytest
 
-from start import jitter
+#from start import jitter
+import zeus.start as jitter
 
 def test_case1(seed=123):
     np.random.seed(seed)
