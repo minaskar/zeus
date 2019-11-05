d85b33413df91b648e527027588caac4b908c18f
minaskar <minaskar@gmail.com>
2019-11-05 17:16:57 +0000

    reduced dims

--- zeus/tests/test_sampler.py
+++ zeus/tests/test_sampler.py
@@ -8,17 +8,17 @@ def logp(x):
 
 
 def test_mean(logp=logp,seed=42):
-    ndim = np.random.randint(2,20)
+    ndim = np.random.randint(2,5)
     nwalkers = 2 * ndim
     nsteps = np.random.randint(500,1000)
     sampler = zeus.sampler(logp,nwalkers,ndim,verbose=False)
     start = np.random.rand(nwalkers,ndim)
     sampler.run(start,nsteps)
-    assert np.all(np.abs(np.mean(sampler.flatten(),axis=0)-1.0)) < 0.05
+    assert np.all(np.abs(np.mean(sampler.flatten(),axis=0)-1.0)) < 0.1
 
 
 def test_std(logp=logp,seed=42):
-    ndim = np.random.randint(2,20)
+    ndim = np.random.randint(2,5)
     nwalkers = 2 * ndim
     nsteps = np.random.randint(500,1000)
     sampler = zeus.sampler(logp,nwalkers,ndim,verbose=False)
