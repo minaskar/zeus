59841b1df29646c0e5cf24943e0cedcd8e569d4e
minaskar <minaskar@gmail.com>
2019-11-05 16:02:43 +0000

    import error

--- zeus/tests/test_fwrapper.py
+++ zeus/tests/test_fwrapper.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 #from zeus import fwrapper
-from zeus import _FunctionWrapper
+from zeus.fwrapper import _FunctionWrapper
 
 def func0(x):
     return - 0.5 * np.sum(x**2.0)
