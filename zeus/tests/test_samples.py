8f05a3c3274f05af9b015805e2c01d8f63ac3dca
minaskar <minaskar@gmail.com>
2019-11-12 16:22:36 +0000

    changed pytest samples

--- zeus/tests/test_samples.py
+++ zeus/tests/test_samples.py
@@ -6,25 +6,27 @@ from zeus import samples
 
 def test_chain(seed=42):
     np.random.seed(seed)
-    s = samples()
     nsteps =np.random.randint(200,400)
     ndim = np.random.randint(2,200)
     nwalkers = 2 * ndim
+    s = samples(ndim, nwalkers)
+    s.extend(nsteps)
     for i in range(nsteps):
         x = np.random.rand(nwalkers,ndim)
-        s.append(x)
+        s.save(x)
     assert np.shape(s.chain) == (nwalkers,nsteps,ndim)
 
 
 def test_flatten(seed=42):
     np.random.seed(seed)
-    s = samples()
     nsteps =np.random.randint(200,400)
     ndim = np.random.randint(2,200)
     nwalkers = 2 * ndim
+    s = samples(ndim,nwalkers)
+    s.extend(nsteps)
     for i in range(nsteps):
         x = np.random.rand(nwalkers,ndim)
-        s.append(x)
+        s.save(x)
     burn = np.random.randint(2,100)
     thin = np.random.randint(1,10)
     assert np.shape(s.flatten(burn,thin)) == ((nsteps-burn)*nwalkers/thin,ndim)
