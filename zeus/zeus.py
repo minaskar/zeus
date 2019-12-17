8509a757b778dfb8b1da2176ad5a0105c52d6579
minaskar <minaskar@gmail.com>
2019-12-17 01:22:19 +0000

    simplicity is a virtue

--- zeus/zeus.py
+++ zeus/zeus.py
@@ -18,7 +18,7 @@ class sampler:
     An ensemble slice MCMC sampler.
 
     Args:
-        logp (callable): A python function that takes a vector in the
+        logprob (callable): A python function that takes a vector in the
             parameter space as input and returns the natural logarithm of the
             unnormalised posterior probability at that position.
         nwalkers (int): The number of walkers in the ensemble.
@@ -27,12 +27,12 @@ class sampler:
         kwargs (list): Extra arguments to be passed into the logp.
         jump (float): Probability of random jump (Default is 0.1). It has to be <1 and >0.
         mu (float): This is the mu coefficient (default value is 3.7). Numerical tests verify this as the optimal choice.
-        parallel (bool): If True (default is False) distribute workload to multiple CPUs.
+        pool (bool): External pool of workers to distribute workload to multiple CPUs (default is None).
         ncores (bool): The maximum number of cores to use if parallel=True (default is None, meaning all of them).
         verbose (bool): If True (default) print log statements.
     """
     def __init__(self,
-                 logp,
+                 logprob,
                  nwalkers,
                  ndim,
                  args=None,
@@ -53,7 +53,7 @@ class sampler:
         else:
             self.logger.setLevel(logging.WARNING)
 
-        self.logp = _FunctionWrapper(logp, args, kwargs)
+        self.logprob = _FunctionWrapper(logprob, args, kwargs)
         self.nwalkers = int(nwalkers)
         self.ndim = int(ndim)
         self.jump = jump
@@ -61,8 +61,6 @@ class sampler:
         self.pool = pool
         self.neval = 0
         self.samples = samples(self.ndim, self.nwalkers)
-        self.X = None
-        self.Z = None
 
 
     def run(self,
@@ -82,27 +80,25 @@ class sampler:
         # Initialise ensemble of walkers
         logging.info('Initialising ensemble of %d walkers...', self.nwalkers)
 
-        self.X = np.copy(start)
-
-        if np.shape(self.X) != (self.nwalkers, self.ndim):
+        if np.shape(start) != (self.nwalkers, self.ndim):
             raise ValueError("Incompatible input dimensions! Please provide array of shape (nwalkers, ndim) as the starting position.")
-
-        self.Z = np.asarray(list(map(self.logp,self.X)))
+        X = np.copy(start)
+        Z = np.asarray(list(map(self.logprob,X)))
 
         batch = list(np.arange(self.nwalkers))
 
         # Extend saving space
         self.nsteps = int(nsteps)
         self.thin = int(thin)
-
         self.samples.extend(self.nsteps//self.thin)
 
+        # Define task distributer
         if self.pool is None:
             distribute = map
         else:
             distribute = self.pool.map
 
-
+        # Initialise progress bar
         if progress:
             t = tqdm(total=nsteps, desc='Sampling progress : ')
 
@@ -120,46 +116,49 @@ class sampler:
             sets = [[batch0,batch1],[batch1,batch0]]
 
             for ensembles in sets:
+                # Define active-inactive sets
                 active, inactive = ensembles
+
+                # Compute Random Pair direction vectors
                 perms = list(permutations(inactive,2))
                 pairs = np.asarray(random.sample(perms,int(self.nwalkers/2))).T
-                directions = self.mu * (self.X[pairs[0]]-self.X[pairs[1]]) * gamma
+                directions = self.mu * (X[pairs[0]]-X[pairs[1]]) * gamma
 
                 mask = np.full(int(self.nwalkers/2),True)
 
-                Zs = self.Z[active] - np.random.exponential(size=int(self.nwalkers/2))
-                Ls = - np.random.uniform(0.0,1.0,size=int(self.nwalkers/2))
-                Rs = Ls + 1.0
+                Z0 = Z[active] - np.random.exponential(size=int(self.nwalkers/2))
+                L = - np.random.uniform(0.0,1.0,size=int(self.nwalkers/2))
+                R = L + 1.0
 
-                x1s = np.empty(int(self.nwalkers/2))
-                logp1s = np.empty(int(self.nwalkers/2))
-                new_vectors = np.empty((int(self.nwalkers/2),self.ndim))
+                Widths = np.empty(int(self.nwalkers/2))
+                Z_prime = np.empty(int(self.nwalkers/2))
+                X_prime = np.empty((int(self.nwalkers/2),self.ndim))
 
                 indeces = np.arange(int(self.nwalkers/2))
 
                 while len(mask[mask])>0:
 
-                    x1s[mask] = Ls[mask] + np.random.uniform(0.0,1.0,size=len(mask[mask])) * (Rs[mask] - Ls[mask])
+                    Widths[mask] = L[mask] + np.random.uniform(0.0,1.0,size=len(mask[mask])) * (R[mask] - L[mask])
 
-                    new_vectors[mask] = directions[mask] * x1s[mask][:,np.newaxis] + self.X[active][mask]
+                    X_prime[mask] = directions[mask] * Widths[mask][:,np.newaxis] + X[active][mask]
 
-                    logp1s[mask] = np.asarray(list(distribute(self.logp,new_vectors[mask])))
+                    Z_prime[mask] = np.asarray(list(distribute(self.logprob,X_prime[mask])))
 
                     self.neval += len(mask[mask])
 
                     for j in indeces[mask]:
-                        if Zs[j] < logp1s[j]:
+                        if Z0[j] < Z_prime[j]:
                             mask[j] = False
-                        if x1s[j] < 0.0:
-                            Ls[j] = x1s[j]
-                        elif x1s[j] > 0.0:
-                            Rs[j] = x1s[j]
+                        if Widths[j] < 0.0:
+                            L[j] = Widths[j]
+                        elif Widths[j] > 0.0:
+                            R[j] = Widths[j]
 
-                self.X[active] = new_vectors
-                self.Z[active] = logp1s
+                X[active] = X_prime
+                Z[active] = Z_prime
 
             if (i+1) % self.thin == 0:
-                self.samples.save(self.X)
+                self.samples.save(X)
             if progress:
                 t.update()
         if progress:
@@ -197,17 +196,6 @@ class sampler:
             2D Flattened chain.
         """
         return self.samples.flatten(burn, thin)
-
-
-    @property
-    def last(self):
-        """
-        Last position of the walkers.
-
-        Returns:
-            (array) : The last position of the walkers.
-        """
-        return self.X
 
 
     @property
