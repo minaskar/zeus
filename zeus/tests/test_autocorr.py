a83364fcd2920505b69e5603acecca3340888afa
minaskar <minaskar@gmail.com>
2019-12-17 13:39:48 +0000

    now fixed

--- zeus/tests/test_autocorr.py
+++ zeus/tests/test_autocorr.py
@@ -15,6 +15,6 @@ def get_chain(seed=42, ndim=5, N=100000):
 def test_1d(seed=42):
     walker0 = get_chain(seed, ndim=1)
     walker1 = get_chain(seed+1, ndim=1)
-    chain = np.vstack((walker0,walker1))
+    chain = np.hstack((walker0,walker1)).T
     tau = _autocorr_time_1d(chain)
-    assert tau < 15.0
+    assert tau < 20.0
