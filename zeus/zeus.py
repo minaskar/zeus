a962440889be4dcbcc10801f5611c07d3a6c4fcf
minaskar <minaskar@gmail.com>
2020-01-08 16:50:29 +0000

    previous jump proposal

--- zeus/zeus.py
+++ zeus/zeus.py
@@ -1,5 +1,5 @@
 import numpy as np
-from itertools import permutations, combinations
+from itertools import permutations
 import random
 from tqdm import tqdm
 import logging
@@ -177,15 +177,8 @@ class sampler:
                     cov = np.cov(X[inactive], rowvar=False)
                     directions = self.mu * np.random.multivariate_normal(mean,cov,size=int(self.nwalkers/2))
                 elif move == 'jump':
-                    #perms = list(permutations(inactive,2))
-                    #pairs = np.asarray(random.sample(perms,int(self.nwalkers/2))).T
-                    #directions = 2.0 * (X[pairs[0]]-X[pairs[1]])
-
-                    combs = np.asarray(list(combinations(inactive,2)))
-                    norms = np.linalg.norm(X[combs.T[0]]-X[combs.T[1]],axis=1)
-                    sorted_indeces = np.flip(np.argsort(norms))
-                    greatest_indeces = sorted_indeces[:int(self.nwalkers/2)]
-                    pairs = combs[greatest_indeces].T
+                    perms = list(permutations(inactive,2))
+                    pairs = np.asarray(random.sample(perms,int(self.nwalkers/2))).T
                     directions = 2.0 * (X[pairs[0]]-X[pairs[1]])
                 elif move == 'random':
                     directions = np.random.normal(0.0,1.0,size=(int(self.nwalkers/2),self.ndim))
