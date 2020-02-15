9f4b5ce51e22f26295898ed418578501816f4695
minaskar <minaskar@gmail.com>
2020-02-15 16:16:31 +0000

    f

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
