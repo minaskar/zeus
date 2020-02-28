988dca707d768822991b408fc6b906cf9b7102e8
minaskar <minaskar@gmail.com>
2020-02-28 20:02:54 +0000

    version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeus-mcmc",
-    version="1.0.1",
+    version="1.0.2",
     author="Minas Karamanis",
     author_email="minaskar@gmail.com",
     description="zeus: Lightning Fast MCMC",
