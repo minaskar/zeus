1eaa32f77c9344a44768cb6be21411c4b884cf11
minaskar <minaskar@gmail.com>
2020-10-29 17:58:46 +0000

    .

--- zeus/tests/test_sampler.py
+++ zeus/tests/test_sampler.py
@@ -12,9 +12,9 @@ def test_mean(logp=logp,seed=42):
     ndim = np.random.randint(2,5)
     nwalkers = 2 * ndim
     nsteps = np.random.randint(3000,5000)
-    sampler = zeus.sampler(nwalkers,ndim,logp,verbose=False)
+    sampler = zeus.EnsembleSampler(nwalkers,ndim,logp,verbose=False)
     start = np.random.rand(nwalkers,ndim)
-    sampler.run(start,nsteps)
+    sampler.run_mcmc(start,nsteps)
     assert np.all(np.abs(np.mean(sampler.get_chain(flat=True),axis=0)-1.0) < 0.1)
     assert np.all(np.isfinite(sampler.get_log_prob(flat=True)))
     assert np.all(np.isfinite(sampler.get_log_prob()))
@@ -25,9 +25,9 @@ def test_std(logp=logp,seed=42):
     ndim = np.random.randint(2,5)
     nwalkers = 2 * ndim
     nsteps = np.random.randint(3000,5000)
-    sampler = zeus.sampler(nwalkers,ndim,logp,verbose=False)
+    sampler = zeus.EnsembleSampler(nwalkers,ndim,logp,verbose=False)
     start = np.random.rand(nwalkers,ndim)
-    sampler.run(start,nsteps)
+    sampler.run_mcmc(start,nsteps)
     assert np.all(np.abs(np.std(sampler.get_chain(flat=True),axis=0)-1.0) < 0.1)
 
 
@@ -44,9 +44,9 @@ def test_ncall(seed=42):
     ndim = 5
     nsteps = 100
     nwalkers = 2 * ndim
-    sampler = zeus.sampler(nwalkers,ndim,loglike,verbose=False)
+    sampler = zeus.EnsembleSampler(nwalkers,ndim,loglike,verbose=False)
     start = np.random.rand(nwalkers,ndim)
-    sampler.run(start,nsteps)
+    sampler.run_mcmc(start,nsteps)
     
     assert loglike.ncalls == sampler.ncall + nwalkers
 
