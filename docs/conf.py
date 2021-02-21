380addc138906ddb48436459d0975d9bf7f2f808
minaskar <minaskar@gmail.com>
2021-02-21 15:31:47 +0000

    .

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2019-2021, Minas Karamanis'
 author = 'Minas Karamanis'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.1'
+release = '2.2.2'
 
 # -- General configuration ---------------------------------------------------
 
