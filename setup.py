f2705285558e4e694056028d7875c3ce6f131da4
minaskar <minaskar@gmail.com>
2023-01-11 19:45:00 -0800

    added github actions

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", 'r') as dependencies:
-    requirements = [pkg.strip() for pkg in dependencies]
-
-setuptools.setup(
-    name="zeus-mcmc",
-    version="2.4.1",
-    author="Minas Karamanis",
-    author_email="minaskar@gmail.com",
-    description="zeus: Lightning Fast MCMC",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/minaskar/zeus",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-    ],
-    install_requires=requirements,
-    python_requires='>=3.6',
-)
+setuptools.setup()
